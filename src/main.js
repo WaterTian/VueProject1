f92f6265b8503fce2f7a8abc461f11075a06a30c
WaterTian <changewater@qq.com>
2018-05-17 12:02:44 +0800

    ..

--- src/main.js
+++ src/main.js
@@ -1,24 +1,13 @@
 import Vue from 'vue'
-import routes from './routes'
+import App from './pages/App'
+import router from './router'
 
-const app = new Vue({
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
   el: '#app',
-  data: {
-    currentRoute: window.location.pathname
-  },
-  computed: {
-    ViewComponent () {
-      const matchingView = routes[this.currentRoute]
-      return matchingView
-        ? require('./pages/' + matchingView + '.vue')
-        : require('./pages/404.vue')
-    }
-  },
-  render (h) {
-    return h(this.ViewComponent)
-  }
-})
-
-window.addEventListener('popstate', () => {
-  app.currentRoute = window.location.pathname
+  router,
+  components: { App },
+  template: '<App/>'
 })
